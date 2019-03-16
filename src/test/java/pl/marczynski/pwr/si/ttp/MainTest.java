d958fac5f51d8818d42c91de0cf68692150a2e88
kp-marczynski <kp.marczynski@gmail.com>
2019-03-16 23:10:20 +0100

    test improvements

--- src/test/java/pl/marczynski/pwr/si/ttp/MainTest.java
+++ src/test/java/pl/marczynski/pwr/si/ttp/MainTest.java
@@ -7,23 +7,33 @@ import pl.marczynski.pwr.si.ttp.genetic.description.DescriptionFileType;
 import pl.marczynski.pwr.si.ttp.genetic.description.ProblemDescription;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
 
 import static org.junit.Assert.assertEquals;
 
 public class MainTest {
 
     @Test
-    public void generateTrivial() {
+    public void shouldGenerateCorrectNumberOfFiles() throws IOException {
         //given
         ProblemDescription trivial = DescriptionFileRepository.getRandomDescriptionForType(DescriptionFileType.TRIVIAL);
         File directory = ResultSaverService.getDirectory(trivial.getFileName());
+        Files.walk(directory.toPath())
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
+        directory = ResultSaverService.getDirectory(trivial.getFileName());
         int numberOfFiles = directory.listFiles().length;
+        assertEquals(0, numberOfFiles);
 
         //when
         new HiperparametersExploration(trivial).explore();
 
         //then
-        assertEquals(numberOfFiles + 30, directory.listFiles().length);
+        assertEquals(60, directory.listFiles().length);
     }
 
 }
